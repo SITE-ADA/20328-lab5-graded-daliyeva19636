08412465708c2e7d225d0ef76dd0feb52e35ca75
daliyeva19636 <daliyeva19636@ada.edu.az>
2026-02-26 10:39:17 +0400

    Add EventServiceImpl:getEventsByTag implementation

--- src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
+++ src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
@@ -85,7 +85,7 @@ public class EventServiceImpl implements EventService {
     // Custom methods
     @Override
     public List<Event> getEventsByTag(String tag) {
-        Add EventServiceImpl:getEventsByTag implementation
+        getEventsByTag
     }
 
     @Override
