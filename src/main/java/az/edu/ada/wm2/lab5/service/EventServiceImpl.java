6aede8576289eb067bb8b5089367c05f3e0a693e
daliyeva19636 <daliyeva19636@ada.edu.az>
2026-02-26 10:51:43 +0400

    Add EventServiceImpl:getEventsByPriceRange implementation

--- src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
+++ src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
@@ -95,8 +95,7 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public List<Event> getEventsByPriceRange(BigDecimal minPrice, BigDecimal maxPrice) {
-       Block 3
-    }
+       Block 3}
 
     @Override
     public List<Event> getEventsByDateRange(LocalDateTime start, LocalDateTime end) {
